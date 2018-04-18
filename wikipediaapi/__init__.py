f664260fe82c7445d81cf3a29973f603d739d58e
Martin Majlis <martin@majlis.cz>
2018-04-18 07:26:26 +0200

    Update version in documentation.

--- wikipediaapi/__init__.py
+++ wikipediaapi/__init__.py
@@ -2,7 +2,7 @@
 Wikipedia-API is easy to use Python wrapper for `Wikipedias'`_ API. It supports extracting texts, sections, links, categories, translations, etc from Wikipedia. Documentation provides code snippets for the most common use cases.
 
 You can learn more at: http://wikipedia-api.readthedocs.io/en/latest/
-Current version is: 0.3.6
+Current version is: "0.3.7"
 '''
 from .wikipedia import *
 __version__ = (0, 3, 7)
