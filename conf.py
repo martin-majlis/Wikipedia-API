3293c8c57636bf6527eee0f9c4c92afb5405dafc
Martin Majlis <martin@majlis.cz>
2018-12-29 09:59:02 +0100

    Include directory with source files

--- conf.py
+++ conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('wikipedia'))
+sys.path.append(os.path.abspath('wikipediaapi'))
 
 
 # -- General configuration ------------------------------------------------
