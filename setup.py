3214e107552b0264f76878d2d6dec636a873838c
Martin Majlis <martin@majlis.cz>
2019-04-07 07:51:19 +0200

    Update version to 0.4.5 for new release.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tests_require = []
 
 setup(
     name='Wikipedia-API',
-    version="0.4.4",
+    version="0.4.5",
     description='Python Wrapper for Wikipedia',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
--- wikipediaapi/__init__.py
+++ wikipediaapi/__init__.py
@@ -6,7 +6,7 @@ from Wikipedia. Documentation provides code snippets for the most common use
 cases.
 """
 
-__version__ = (0, 4, 4)
+__version__ = (0, 4, 5)
 import logging
 import re
 from enum import IntEnum
