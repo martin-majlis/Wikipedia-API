ee4c2cd966717ab628175b47adcade214eae936c
Martin Majlis <martin@majlis.cz>
2019-04-07 12:10:26 +0200

    Update version to 0.5.1 for new release.

--- wikipediaapi/__init__.py
+++ wikipediaapi/__init__.py
@@ -6,7 +6,7 @@ from Wikipedia. Documentation provides code snippets for the most common use
 cases.
 """
 
-__version__ = (0, 5, 0)
+__version__ = (0, 5, 1)
 import logging
 import re
 from enum import IntEnum
