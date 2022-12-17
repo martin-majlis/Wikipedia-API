652f90d24c6803fe780742e1996598d8259f2e56
Martin Majlis <martin@majlis.cz>
2022-12-17 18:23:20 +0100

    Update version to 0.5.8 for new release.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_require = []  # type: List[str]
 
 setup(
     name="Wikipedia-API",
-    version="0.5.7",
+    version="0.5.8",
     description="Python Wrapper for Wikipedia",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
