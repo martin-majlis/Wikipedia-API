ad0da5b560de4577b6f7571c1e5e46e7fc35a685
Martin Majlis <martin@majlis.cz>
2024-08-24 16:13:32 +0200

    Update version to 0.7.1 for new release.

--- wikipediaapi/__init__.py
+++ wikipediaapi/__init__.py
@@ -6,7 +6,7 @@ from Wikipedia. Documentation provides code snippets for the most common use
 cases.
 """
 
-__version__ = (0, 7, 0)
+__version__ = (0, 7, 1)
 
 from collections import defaultdict
 from enum import IntEnum
